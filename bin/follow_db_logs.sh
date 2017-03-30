0e9ffc12a642d3e7d47a602f1b8eab3c655bd9e3
Ruslan Talpa <ruslan.talpa@gmail.com>
2017-03-30 11:19:31 +0300

    color for db logs 2

--- bin/follow_db_logs.sh
+++ bin/follow_db_logs.sh
@@ -2,5 +2,5 @@
 # BASEDIR=`realpath $(dirname $(realpath $0))/../`
 BASEDIR=/project
 source $BASEDIR/.env
-STYLE=${1:monokai}
+STYLE=${1:default}
 docker logs -f ${COMPOSE_PROJECT_NAME}_db_1 2>&1 | pygmentize -O style=${STYLE} -l postgresql  -f 256 -s
