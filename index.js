fbcf3e953f5f7c0c6f029eb7fb0ebb8f77f7c6cf
SteveBash <ioexception02@gmail.com>
2017-04-17 22:10:49 -0500

    Add watcher to log visualizer

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-require('dotenv').config(); //.env file vars added to process.env 
+require('dotenv').config(); //.env file vars added to process.env
 
 const blessed   = require('blessed')
 const proc      = require('child_process');
 const highlight = require('cli-highlight').highlight;
 const decoder   = new (require('string_decoder').StringDecoder)('utf8');
+const watcher = require('chokidar');
 
 const COMPOSE_PROJECT_NAME = process.env.COMPOSE_PROJECT_NAME;
 
@@ -24,7 +25,7 @@ let pgLogger = blessed.log({
   top: 0,
   left: 0,
   width: '60%',
-  height: '100%',
+  height: '80%',
   border: 'line',
   keys: true,
   vi: true,
@@ -49,7 +50,7 @@ let openRestyLogger = blessed.log({
   top: 0,
   left: '60%',
   width: '40%',
-  height: '100%',
+  height: '80%',
   border: 'line',
   keys: true,
   vi: true,
@@ -68,6 +69,40 @@ oRestyLogsProc.stdout.on('data', (data) => {
            .replace(/<span class=\"hljs-string\">/g, '').replace(/<\/span>/g, '');
   openRestyLogger.log(res);
 });
+
+let pgReloaderLogger = blessed.log({
+  label: "File Watcher",
+  parent: screen,
+  top: "80%",
+  left: 0,
+  width: '100%',
+  height: '20%',
+  border: 'line',
+  keys: true,
+  vi: true,
+  mouse: true,
+  scrollback: 100,
+  scrollbar: {
+    style: {
+      inverse: true
+    }
+  }
+});
+
+const POSTGRES_USER = process.env.POSTGRES_USER;
+const DB_NAME = process.env.DB_NAME;
+const DB_DIR = "docker-entrypoint-initdb.d/";
+
+let pgReloaderProc = path => proc.spawn('docker',['exec', `${COMPOSE_PROJECT_NAME}_db_1`, 'psql', '-U', POSTGRES_USER, DB_NAME, '-c', `\\i ${path}`]);
+
+watcher.watch(['sql/**/*.sql'], {persistent: true})
+	.on('change', path => {
+		let proc = pgReloaderProc(path.replace('sql/', DB_DIR));
+		pgReloaderLogger.log(`${path} changed`);
+		proc.stdout.on('data', data => pgReloaderLogger.log(highlight(decoder.write(data), { language : 'sql'})));
+		proc.stderr.on('data', data => pgReloaderLogger.log(highlight(decoder.write(data), { language : 'sql'})));
+	})
+	.on('ready', () => pgReloaderLogger.log('Watching sql/ directory for changes'));
 
 screen.render();
 
