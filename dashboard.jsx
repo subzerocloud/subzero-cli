22e94186727915cc99207173fc0471dc9a870578
SteveBash <ioexception02@gmail.com>
2017-04-22 17:48:53 -0500

    Add restart this container action

--- dashboard.jsx
+++ dashboard.jsx
@@ -24,55 +24,81 @@ if(!COMPOSE_PROJECT_NAME){
   process.exit(0);
 }
 
-const decoder = new StringDecoder('utf8');
-const prettyPrint = (data, lang) => highlight(decoder.write(data), {language : lang});
+const PG = `${COMPOSE_PROJECT_NAME}_db_1`
+const PGREST = `${COMPOSE_PROJECT_NAME}_postgrest_1`;
+const RMQ = `${COMPOSE_PROJECT_NAME}_rabbitmq_1`;
+const OPENRESTY = `${COMPOSE_PROJECT_NAME}_openresty_1`;
 
+//Processes that output the full log, only used on start
 const logsProc = container => proc.spawn('docker',['logs', '-f', container]);
-const pgLogsProc = logsProc(`${COMPOSE_PROJECT_NAME}_db_1`);
-const pgRestLogsProc = logsProc(`${COMPOSE_PROJECT_NAME}_postgrest_1`);
-const oRestyLogsProc = logsProc(`${COMPOSE_PROJECT_NAME}_openresty_1`);
-const rmqLogsProc = logsProc(`${COMPOSE_PROJECT_NAME}_rabbitmq_1`);
+const pgLogsProc = logsProc(PG);
+const pgRestLogsProc = logsProc(PGREST);
+const oRestyLogsProc = logsProc(OPENRESTY);
+const rmqLogsProc = logsProc(RMQ);
+
+//Processes that output the tail of log, used for showing the logs after container restart to avoid showing the log from the beginning
+const logsTailProc = container => proc.spawn('docker',['logs', '-f', '--tail=1', container]);
+let pgTailProc = null;
+let pgRestTailProc = null;
+let oRestyTailProc = null;
+let rmqTailProc = null;
 
 const pgWatcher = chokidar.watch('sql/**/*.sql');
 const luaWatcher = chokidar.watch('lua/**/*.lua');
 const nginxWatcher = chokidar.watch('nginx/**/*.conf');
 
-const pgReloaderProc = path => proc.spawn('docker',['exec', `${COMPOSE_PROJECT_NAME}_db_1`, 'psql', '-U', POSTGRES_USER, DB_NAME, '-c', `\\i ${path}`]);
-const nginxHupperProc = () => proc.spawn('docker',['kill', '-s', 'HUP', `${COMPOSE_PROJECT_NAME}_openresty_1`]);
+const pgReloaderProc = path => proc.spawn('docker',['exec', PG, 'psql', '-U', PG_USER, DB_NAME, '-c', `\\i ${path}`]);
+const nginxHupperProc = () => proc.spawn('docker',['kill', '-s', 'HUP', OPENRESTY]);
 
-const restartProc = container => proc.spawn('docker',['restart', container]);
+const restartProc = (container, succ, err) => {
+  let p = proc.spawn('docker',['restart', container]);
+  p.stdout.on('data', succ);
+  p.stderr.on('data', err);
+}
+
+const decoder = new StringDecoder('utf8');
+const prettyPrint = (data, lang) => highlight(decoder.write(data), {language : lang});
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-			hiddenLogs : [false, true, true, true]
+      curIdx : 0,
+			hidden : [false, true, true, true],
+      restarted : [false, false, false, false]
 		};
   }
   handleKeyPress = (ch, key) => {
     const k = key.name;
     if(k == "left" || k == "right"){
-      const {hiddenLogs} = this.state;
+      const {hidden} = this.state;
       // Hide current log
-      let idx = hiddenLogs.findIndex( x => !x );
-      hiddenLogs[idx] = true;
+      let idx = hidden.findIndex( x => !x );
+      hidden[idx] = true;
       // Show next log and iterate circularly
       if(k == "left")
-        idx = (idx - 1 == -1)?(hiddenLogs.length - 1):(idx - 1);
+        idx = (idx - 1 == -1)?(hidden.length - 1):(idx - 1);
       if(k == "right")
-        idx = (idx + 1 == hiddenLogs.length)?0:(idx + 1);
-      hiddenLogs[idx] = false;
-      this.setState({ hiddenLogs: hiddenLogs });
+        idx = (idx + 1 == hidden.length)?0:(idx + 1);
+      hidden[idx] = false;
+      this.setState({hidden: hidden, curIdx: idx});
+    }
+    if(ch == '2'){
+      const {curIdx, restarted} = this.state;
+      restarted[curIdx] = true;
+      this.setState({restarted : restarted});
+      // Workaround to prevent re-restarting
+      this.setState({restarted : [false, false, false, false]});
     }
   }
   render() {
-    const { hiddenLogs, message } = this.state;
+    const { hidden, restarted } = this.state;
     return (
       <element keyable={true} onKeypress={this.handleKeyPress}>
-        <PgLog hidden={hiddenLogs[0]}/>
-        <ORestyLog hidden={hiddenLogs[1]}/>
-        <PgRESTLog hidden={hiddenLogs[2]}/>
-        <RMQLog hidden={hiddenLogs[3]}/>
+        <PgLog restart={restarted[0]} hidden={hidden[0]}/>
+        <ORestyLog restart={restarted[1]} hidden={hidden[1]}/>
+        <PgRESTLog restart={restarted[2]} hidden={hidden[2]}/>
+        <RMQLog restart={restarted[3]} hidden={hidden[3]}/>
         <WatcherLog/>
         <Options/>
       </element>
@@ -96,7 +122,6 @@ class Options extends Component {
 		super(props);
 	}
   render(){
-    const {hidden} = this.props;
     return (
       <layout top="95%" width="100%" height="5%">
         <button class={buttonStyle} content="1: Purge log"/>
@@ -131,13 +156,26 @@ class PgLog extends Component {
 	constructor(props){
 		super(props);
 	}
+  componentWillReceiveProps(nextProps){
+    const logger = this.refs.pgLog;
+    if(nextProps.restart){
+      logger.log(`Restarting ${PG} ...`);
+      restartProc(PG, 
+        data => {
+          logger.log("Done");
+          pgTailProc = logsTailProc(PG);
+          pgTailProc.stderr.on('data', data => logger.log(prettyPrint(data, 'sql')));
+        },
+        data => logger.log(prettyPrint(data, 'accessslog')));
+    }
+  }
 	componentDidMount(){
 		pgLogsProc.stderr.on('data', data => this.refs.pgLog.log(prettyPrint(data, 'sql')) );
 	}
   render(){
     const {hidden} = this.props;
     return (
-      <log hidden={hidden} ref="pgLog" label="Logs PostgreSQL" class={logStyle}
+      <log onRestart={this.handleRestart} hidden={hidden} ref="pgLog" label="Logs PostgreSQL" class={logStyle}
            width="70%" height="95%"/>
     );
   }
@@ -147,8 +185,25 @@ class ORestyLog extends Component {
 	constructor(props){
 		super(props);
 	}
+  componentWillReceiveProps(nextProps){
+    const logger = this.refs.oRestyLog;
+    if(nextProps.restart){
+      logger.log(`Restarting ${OPENRESTY} ...`);
+      restartProc(OPENRESTY, 
+        data => {
+          logger.log("Done");
+          oRestyTailProc = logsTailProc(OPENRESTY);
+          oRestyTailProc.stdout.on('data', data => logger.log(prettyPrint(data, 'accesslog')));
+          oRestyTailProc.stderr.on('data', data => logger.log(prettyPrint(data, 'accesslog')));
+        },
+        data => logger.log(prettyPrint(data, 'accessslog')));
+      
+    }
+  }
 	componentDidMount(){
-		oRestyLogsProc.stdout.on('data', data => this.refs.oRestyLog.log(prettyPrint(data, 'accesslog')));
+    const logger = this.refs.oRestyLog;
+		oRestyLogsProc.stdout.on('data', data => logger.log(prettyPrint(data, 'accesslog')));
+		oRestyLogsProc.stderr.on('data', data => logger.log(prettyPrint(data, 'accesslog')));
 	}
   render() {
     const {hidden} = this.props;
@@ -163,6 +218,20 @@ class RMQLog extends Component {
 	constructor(props){
 		super(props);
 	}
+  componentWillReceiveProps(nextProps){
+    const logger = this.refs.rmqLog;
+    if(nextProps.restart){
+      logger.log(`Restarting ${RMQ} ...`);
+      restartProc(RMQ, 
+        data => {
+          logger.log("Done");
+          rmqTailProc = logsTailProc(RMQ);
+          rmqTailProc.stdout.on('data', data => logger.log(prettyPrint(data, 'accesslog')));
+          rmqTailProc.stderr.on('data', data => logger.log(prettyPrint(data, 'accesslog')));
+        },
+        data => logger.log(prettyPrint(data, 'accessslog')));
+    }
+  }
 	componentDidMount(){
 		rmqLogsProc.stdout.on('data', data => this.refs.rmqLog.log(prettyPrint(data, 'accesslog')));
 	}
@@ -179,6 +248,19 @@ class PgRESTLog extends Component {
 	constructor(props){
 		super(props);
 	}
+  componentWillReceiveProps(nextProps){
+    const logger = this.refs.pgRestLog;
+    if(nextProps.restart){
+      logger.log(`Restarting ${PGREST} ...`);
+      restartProc(PGREST, 
+        data => {
+          logger.log("Done");
+          pgRestTailProc = logsTailProc(PGREST);
+          pgRestTailProc.stdout.on('data', data => logger.log(prettyPrint(data, 'accesslog')));
+        },
+        data => logger.log(prettyPrint(data, 'accessslog')));
+    }
+  }
 	componentDidMount(){
 		pgRestLogsProc.stdout.on('data', data => this.refs.pgRestLog.log(prettyPrint(data, 'accesslog')));
 	}
@@ -199,24 +281,24 @@ class WatcherLog extends Component {
     let logger = this.refs.watcherLog;
     pgWatcher
       .on('change', path => {
-        let proc = pgReloaderProc(path.replace('sql/', DB_DIR));
+        let p = pgReloaderProc(path.replace('sql/', DB_DIR));
         logger.log(`${path} changed`);
-        proc.stdout.on('data', data => logger.log(prettyPrint(data, 'sql')));
-        proc.stderr.on('data', data => logger.log(prettyPrint(data, 'sql')));
+        p.stdout.on('data', data => logger.log(prettyPrint(data, 'sql')));
+        p.stderr.on('data', data => logger.log(prettyPrint(data, 'sql')));
       })
       .on('ready', () => logger.log('Watching sql/ directory for changes'));
     luaWatcher
       .on('change', path => {
-        let proc = nginxHupperProc();
+        let p = nginxHupperProc();
         logger.log(`${path} changed`);
-        proc.stdout.on('data', data => logger.log("Nginx restarted"));
+        p.stdout.on('data', data => logger.log("Nginx restarted"));
       })
       .on('ready', () => logger.log('Watching lua/ directory for changes'));
     nginxWatcher
       .on('change', path => {
-        let proc = nginxHupperProc();
+        let p = nginxHupperProc();
         logger.log(`${path} changed`);
-        proc.stdout.on('data', data => logger.log("Nginx restarted"));
+        p.stdout.on('data', data => logger.log("Nginx restarted"));
       })
       .on('ready', () => logger.log('Watching nginx/ directory for changes'));
 	}
@@ -244,6 +326,10 @@ process.on('exit', () => {
   pgRestLogsProc.kill();
   oRestyLogsProc.kill();
   rmqLogsProc.kill();
+  if(pgTailProc) pgTailProc.kill();
+  if(pgRestTailProc) pgRestTailProc.kill();
+  if(oRestyTailProc) oRestyTailProc.kill();
+  if(rmqTailProc) rmqTailProc.kill();
   pgWatcher.close();
   luaWatcher.close();
   nginxWatcher.close();
