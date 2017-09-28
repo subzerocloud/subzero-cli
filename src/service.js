36d7294e65ca81e851c411f4bfedec20054fa90c
steve-chavez <stevechavezast@gmail.com>
2017-09-28 12:03:15 -0500

    Add rest request and saving token to file for login

--- src/service.js
+++ src/service.js
@@ -1,34 +1,70 @@
 #!/usr/bin/env node
 "use strict";
 
+import os from 'os';
+import fs from 'fs';
 import program from 'commander';
 import inquirer from 'inquirer';
+import request from 'superagent';
+
+const SERVER_URL = "http://localhost:3000";
+
+const HOME_DIR = os.homedir();
+const SUBZERO_DIR = `${HOME_DIR}/.subzero`
+const SUBZERO_CREDENTIALS_FILE = "credentials.json"
 
 const login = (username, password) => {
-  console.log("Welcome " + username);
+  request
+    .post(`${SERVER_URL}/rpc/login`)
+    .send({"email": username, "password": password})
+    .end((err, res) => {
+      if(res.ok){
+        if(!fs.existsSync(SUBZERO_DIR)) {
+          fs.mkdirSync(SUBZERO_DIR);
+        }
+        fs.writeFileSync(`${SUBZERO_DIR}/${SUBZERO_CREDENTIALS_FILE}`,
+                         `{ token: "${res.body[0].token}" }`);
+        console.log("\x1b[32m%s\x1b[0m", "Login succeeded");
+      }else
+        console.log("\x1b[31m%s\x1b[0m", res.body.message);
+    });
 }
+
+// options.key returns bool if a value is not specified(e.g. subzero service login -u -p, options.{username,password} gives true), so make sure is a string
+const notEmptyString = s => (typeof s == 'string')&&s.trim().length;
+
 program
   .command('login')
   .option("-u, --username [username]", "Your username")
   .option("-p, --password [password]", "Your password")
   .description('Login to subzero')
   .action(options => {
-    if(!options.username && !options.password)
+    const username = options.username;
+    const password = options.password;
+    if(!username && !password)
       inquirer.prompt([
         {
           type: 'input',
           message: "Enter your username",
-          name: 'username'
+          name: 'username',
+          validate: val => notEmptyString(val)?true:"Please enter your username"
         },
         {
           type: 'password',
-          message: 'Enter a masked password',
+          message: 'Enter your password',
           name: 'password',
-          mask: '*'
+          mask: '*',
+          validate: val => notEmptyString(val)?true:"Please enter your password"
         }
       ]).then(answers => login(answers.username, answers.password));
-    else
-      login(options.username, options.password);
+    else{
+      if(!notEmptyString(username))
+        console.log("username: cannot be empty");
+      if(!notEmptyString(password))
+        console.log("password: cannot be empty");
+      if(notEmptyString(username) && notEmptyString(password))
+        login(username, password);
+    }
   });
 
 program.parse(process.argv);
