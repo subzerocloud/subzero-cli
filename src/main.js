8db7d7b0cd00a46a227983d327c01cc9e2b115e8
steve-chavez <stevechavezast@gmail.com>
2017-10-02 15:52:39 -0500

    Add base-project command

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 
 import program from 'commander';
 import {version} from '../package.json';
+import proc from 'child_process';
+import inquirer from 'inquirer';
 
 program
   .version(version)
@@ -20,7 +22,35 @@ program.on('--help', function(){
   console.log('    PSQL_CMD: psql');
   console.log('    PG_DUMP_CMD: pg_dump');
   console.log('    PG_DUMPALL_CMD: pg_dumpall');
-  
   console.log('');
 });
+
+program
+  .command('base-project')
+  .description('Download a base project')
+  .action(() => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        message: "Enter a dir(Use '.' for current dir)",
+        name: 'dir',
+        validate: val => notEmptyString(val)?true:"Please enter a dir"
+      }
+    ]).then(answers => baseProject(answers.dir));
+  });
+
+const notEmptyString = s => (typeof s == 'string')&&s.trim().length;
+
+const baseProject = dir => {
+  let p   = proc.spawnSync("git", ["clone", "https://github.com/subzerocloud/postgrest-starter-kit", dir]),
+      out = p.stdout.toString(),
+      err = p.stderr.toString();
+  if(err){
+    console.log(err);
+    process.exit(1);
+  } else {
+    console.log(out);
+  }
+}
+
 program.parse(process.argv);
